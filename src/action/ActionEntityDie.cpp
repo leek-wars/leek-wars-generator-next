8b345f2e78c515712fcaa66eef79539ddc92e970
Pilow <pierre.laupretre@gmail.com>
2016-08-28 21:46:23 +0200

    Add action

--- src/action/ActionEntityDie.cpp
+++ src/action/ActionEntityDie.cpp
@@ -1,25 +1,12 @@
-package com.leekwars.game.fight.action;
+#include "ActionEntityDie.hpp"
+#include "../entity/Entity.hpp"
 
-import com.alibaba.fastjson.JSONArray;
-import com.leekwars.game.fight.entity.Entity;
-
-public class ActionEntityDie implements Action {
-
-	private final int id;
-	private final int killer;
-
-	public ActionEntityDie(Entity leek, Entity killer) {
-		this.id = leek.getFId();
-		this.killer = killer != null ? killer.getFId() : -1;
-	}
-
-	@Override
-	public JSONArray getJSON() {
-
-		JSONArray retour = new JSONArray();
-		retour.add(Action.PLAYER_DEAD);
-		retour.add(id);
-		retour.add(killer);
-		return retour;
-	}
+ActionEntityDie::ActionEntityDie(Entity* leek, Entity* killer) {
+	this->id = leek->id;
+	this->killer = killer != nullptr ? killer->id : -1;
 }
+
+Json ActionEntityDie::json() const {
+	return {(int) ActionType::PLAYER_DEAD, id, killer};
+}
+
