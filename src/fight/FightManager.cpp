e42fe0235556355ab8e3f3ada3ee0c4955c91a7d
Pilow <pierre.laupretre@gmail.com>
2017-05-09 23:31:11 +0200

    [fight] Add entity module static methods as global variables

--- src/fight/FightManager.cpp
+++ src/fight/FightManager.cpp
@@ -9,6 +9,7 @@
 #include "../module/ChipModule.hpp"
 
 FightManager::FightManager() : vm(), vm_v1(true) {
+	// V2
 	vm.add_module(new FightModule());
 	vm.add_module(new EntityModule());
 	vm.add_module(new LeekModule());
@@ -17,14 +18,26 @@ FightManager::FightManager() : vm(), vm_v1(true) {
 	vm.add_module(new WeaponModule());
 	vm.add_module(new ColorModule());
 	vm.add_module(new ChipModule());
+
+	// V1
+	auto entity_module = new EntityModule();
 	vm_v1.add_module(new FightModule());
-	vm_v1.add_module(new EntityModule());
+	vm_v1.add_module(entity_module);
 	vm_v1.add_module(new LeekModule());
 	vm_v1.add_module(new MapModule());
 	vm_v1.add_module(new CellModule());
 	vm_v1.add_module(new WeaponModule());
 	vm_v1.add_module(new ColorModule());
 	vm_v1.add_module(new ChipModule());
+
+	for (auto& method : entity_module->static_methods) {
+		if (method.name.at(0) == '_') {
+			auto fun = new ls::LSFunction<ls::LSValue*>(method.impl[0].addr);
+			fun->refs = 1;
+			fun->native = true;
+			vm_v1.add_constant(method.name.substr(1), method.impl[0].type, fun);
+		}
+	}
 }
 
 Report* FightManager::start(Fight& fight) {
