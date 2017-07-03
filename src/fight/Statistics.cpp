bb967562ac308fb65e4cb5a6ddcb9162aa6737da
Pilow <pierre.laupretre@gmail.com>
2017-07-03 23:34:34 +0200

    [statistics] Remove constructor and destructor

--- src/fight/Statistics.cpp
+++ src/fight/Statistics.cpp
@@ -1,22 +1,6 @@
-/*
- * Statistics.cpp
- *
- *  Created on: May 19, 2016
- *      Author: pierre
- */
-
 #include "Statistics.hpp"
 
 #include "../entity/Entity.hpp"
-
-Statistics::Statistics() {
-	// TODO Auto-generated constructor stub
-
-}
-
-Statistics::~Statistics() {
-	// TODO Auto-generated destructor stub
-}
 
 void Statistics::add_damage(Entity* attacker, int damage, Entity* target) {
 
--- src/fight/Statistics.hpp
+++ src/fight/Statistics.hpp
@@ -11,9 +11,6 @@ public:
 	std::map<int, int> damages;
 	std::map<int, int> heals;
 
-	Statistics();
-	virtual ~Statistics();
-
 	void add_damage(Entity* attacker, int damage, Entity* target);
 };
 
