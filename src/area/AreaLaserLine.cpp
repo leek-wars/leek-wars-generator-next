ab1dc5b3433d0de960e5aef780c73edb3bae9c03
Pilow <pierre.laupretre@gmail.com>
2017-07-04 21:40:36 +0200

    [area] Implement AreaLaserLine

--- src/area/AreaLaserLine.cpp
+++ src/area/AreaLaserLine.cpp
@@ -4,8 +4,33 @@ AreaLaserLine::AreaLaserLine(Attack* attack) : Area(attack) {}
 
 AreaLaserLine::~AreaLaserLine() {}
 
-std::vector<Cell*> AreaLaserLine::getArea(Cell*, Cell*) const {
-	return {};
+std::vector<Cell*> AreaLaserLine::getArea(Cell* launch_cell, Cell* target_cell) const {
+	std::vector<Cell*> cells;
+	int dx = 0, dy = 0;
+	if (launch_cell->x == target_cell->x) {
+		if (launch_cell->y > target_cell->y) {
+			dy = -1;
+		} else {
+			dy = 1;
+		}
+	} else if (launch_cell->y == target_cell->y) {
+		if (launch_cell->x > target_cell->x) {
+			dx = -1;
+		} else {
+			dx = 1;
+		}
+	} else {
+		return cells;
+	}
+	int x = launch_cell->x, y = launch_cell->y;
+	for (int i = attack->min_range; i <= attack->max_range; i++) {
+		auto* c = launch_cell->map->getCell(x + dx * i, y + dy * i);
+		if (c == nullptr or !c->walkable) {
+			break;
+		}
+		cells.push_back(c);
+	}
+	return cells;
 }
 
 int AreaLaserLine::getRadius() const {
