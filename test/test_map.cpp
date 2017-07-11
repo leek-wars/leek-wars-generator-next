4c32c586f584cacef60e77a23125e5539e6ea150
Pilow <pierre.laupretre@gmail.com>
2017-07-11 23:13:00 +0200

    [test] Rename test_map test case

--- test/test_map.cpp
+++ test/test_map.cpp
@@ -1,4 +1,5 @@
 #include "Test.hpp"
+#include "../benchmark/Benchmark.hpp"
 
 void Test::test_map() {
 
@@ -8,8 +9,6 @@ void Test::test_map() {
 	Map map(18, 18, 100, {});
 
 	map.print();
-
-	auto exe_start = std::chrono::high_resolution_clock::now();
 
 	for (int i = 0; i < 100; ++i) {
 		int c1 = rand() % 613;
@@ -31,8 +30,6 @@ void Test::test_map() {
 	auto path2 = map.get_path_between(map.cells[c3], map.cells[c4], {});
 	map.draw_path(path2, map.int_to_cell(c3), map.int_to_cell(c4));
 
-	auto exe_end = std::chrono::high_resolution_clock::now();
-	long exe_time_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(exe_end - exe_start).count();
-	double exe_time_ms = (((double) exe_time_ns / 1000) / 1000);
-	std::cout << "pathfinding: " << exe_time_ms << " ms" << std::endl;
+	header("Pathfinding benchmark");
+	Benchmark::pathfinding();
 }
