fba2dff5f5482d864853c2f4ee96e366416cc07d
Pilow <pierre.laupretre@gmail.com>
2017-07-11 23:13:00 +0200

    [entity] Reorganize entity header methods

--- src/entity/Entity.hpp
+++ src/entity/Entity.hpp
@@ -75,33 +75,41 @@ public:
 	Entity(Fight* fight, std::string name, int level);
 	virtual ~Entity();
 
+	/*
+	 * General information
+	 */
 	Team* getTeam() const;
-
 	bool isAlive() const;
 	bool isDead() const;
 	bool isAlly() const;
 	bool isEnemy() const;
 	bool isSummon() const;
-	void addLife(int l);
-	void removeLife(int life, Entity* attacker);
-
 	void setCell(Cell* cell);
 	Cell* getCell();
+	int getLevel() const;
+	ls::LSString* getName() const;
 
+	/*
+	 * Items
+	 */
 	void setWeapons(std::vector<Weapon*> weapons);
 	const Weapon* getWeapon();
 	ls::LSArray<ls::LSValue*>* getWeapons();
 	bool setWeapon(const Weapon* weapon);
 	bool setWeaponInteger(int weapon);
-
 	void setChips(std::vector<Chip*> chips);
 	ls::LSArray<ls::LSValue*>* getChips();
 
+	/*
+	 * Characteristics
+	 */
+ 	void addLife(int l);
+ 	void removeLife(int life, Entity* attacker);
 	void setCharacteristics(Characteristics characs);
+	int getCharacteristic(Characteristic characteristic) const;
 	void updateCharacteristics();
 	void updateBonusCharacteristics();
 	void updateBonusCharacteristics(Characteristic charac);
-
 	int getTP() const;
 	int getMP() const;
 	int getTotalTP() const;
@@ -118,35 +126,28 @@ public:
 	int getDamageReturn() const;
 	int getAbsoluteShield() const;
 	int getRelativeShield() const;
-	int getCharacteristic(Characteristic characteristic) const;
-	int getLevel() const;
-	ls::LSString* getName() const;
 
+	/*
+	 * Fight actions
+	 */
+	void useTP(int tp);
+	void useMP(int mp);
+	void endTurn();
 	int move(const std::vector<const Cell*> path);
+	int moveToward(Entity* target);
+	int moveTowardMP(Entity* target, int max_mp);
 	int useChip(Chip* chip, Entity* target);
 	int useChipOnCell(Chip* chip, Cell* cell);
 	int useWeapon(Entity* target);
 	int useWeaponOnCell(Cell* cell);
-
-	std::vector<Entity*> get_enemies() const;
-	std::vector<Entity*> get_allies() const;
-	Entity* get_closest(std::vector<Entity*> entities) const;
-	Entity* get_closest_enemy() const;
-	Entity* get_closest_ally() const;
-
-	void useTP(int tp);
-	void useMP(int mp);
-	void endTurn();
-
-	bool say(const ls::LSValue* message);
-
-	int moveToward(Entity* target);
-	int moveTowardMP(Entity* target, int max_mp);
-
 	void addCooldown(Chip* chip, int cooldown);
 	bool hasCooldown(int chipID) const;
 	int getCooldown(int chipID);
+	bool say(const ls::LSValue* message);
 
+	/*
+	 * Effects management
+	 */
 	ls::LSArray<ls::LSValue*> getEffects();
 	ls::LSArray<ls::LSValue*> getLaunchedEffects();
 	void addEffect(Effect* effect);
@@ -155,10 +156,24 @@ public:
 	void remove_launched_effect(Effect* effect);
 	void clear_poisons();
 
-	virtual ls::LSValue* getClass() const override;
+	/*
+	 * Entity getters
+	 */
+	std::vector<Entity*> get_enemies() const;
+	std::vector<Entity*> get_allies() const;
+	Entity* get_closest(std::vector<Entity*> entities) const;
+	Entity* get_closest_enemy() const;
+	Entity* get_closest_ally() const;
 
+	/*
+	 * LeekScript overrides
+	 */
+	virtual ls::LSValue* getClass() const override;
 	virtual std::ostream& print(std::ostream& os) const override;
 
+	/*
+	 * Export to JSON format
+	 */
 	Json to_json() const;
 };
 
