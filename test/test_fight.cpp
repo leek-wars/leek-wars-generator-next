4cdee55f72599d9761f5282532e4e0ebd2c5b9b3
Pilow <pierre.laupretre@gmail.com>
2017-07-12 22:32:48 +0200

    [test][fight] Use the new function test_ai

--- test/test_fight.cpp
+++ test/test_fight.cpp
@@ -3,67 +3,15 @@
 
 void Test::test_fight() {
 
-	header("Fight");
+	header("Fight > general");
+	Util::log_enabled = false;
 
-	Fight fight;
-
-	Team* team1 = new Team(0);
-	// Entity 1
-	AI* ai1 = new AI(Util::read_file("test/ai/color.ls"), "color.ls");
-	Leek* leek1 = new Leek(&fight, "Trevor", 300, ai1);
-	Characteristics characs1;
-	characs1.set(Characteristic::LIFE, 3200);
-	characs1.set(Characteristic::TP, 2000);
-	characs1.set(Characteristic::MP, 10);
-	characs1.set(Characteristic::STRENGTH, 978);
-	leek1->setCharacteristics(characs1);
-	std::vector<Weapon*> weapons1 = {manager.weapons["pistol"], manager.weapons["laser"]};
-	leek1->setWeapons(weapons1);
-	std::vector<Chip*> chips1 = {manager.chips["fortress"]};
-	leek1->setChips(chips1);
-	team1->add_entity(leek1);
-	leek1->team = team1;
-
-	Team* team2 = new Team(1);
-	// Entity 2
-	AI* ai2 = new AI(Util::read_file("test/ai/hangry.ls"), "hangry.ls");
-	Leek* leek2 = new Leek(&fight, "Franklin", 297, ai2);
-	Characteristics characs2;
-	characs2.set(Characteristic::LIFE, 3900);
-	characs2.set(Characteristic::TP, 2000);
-	characs2.set(Characteristic::MP, 10);
-	characs2.set(Characteristic::STRENGTH, 470);
-	leek2->setCharacteristics(characs2);
-	std::vector<Weapon*> weapons2 = {manager.weapons["laser"]};
-	leek2->setWeapons(weapons2);
-	std::vector<Chip*> chips2 = {manager.chips["fortress"]};
-	leek2->setChips(chips2);
-	team2->add_entity(leek2);
-	leek2->team = team2;
-	// Entity 3
-	auto ai3 = new AI(Util::read_file("test/ai/basic.ls"), "basic.ls");
- 	auto leek3 = new Leek(&fight, "Hodor", 297, ai3);
-	Characteristics characs3;
-	characs3.set(Characteristic::LIFE, 3900);
-	characs3.set(Characteristic::TP, 2000);
-	characs3.set(Characteristic::MP, 10);
-	characs3.set(Characteristic::STRENGTH, 470);
-	leek3->setCharacteristics(characs3);
-	leek3->setWeapons({manager.weapons["pistol"], manager.weapons["laser"]});
-	leek3->setChips({manager.chips["fortress"]});
-	team2->add_entity(leek3);
-	leek3->team = team2;
-
-	fight.teams = {team1, team2};
-
-	fight.field.reset(new Field(18, 18, 25, {team1, team2}));
-
-	// Run the fight
-	manager.start(fight, [](Report* report) {
-		std::cout << "-------------- report ----------------" << std::endl;
-		std::cout << report << std::endl;
-		delete report;
-	});
+	auto fight = FightLoader::load(manager, "test/fight/fight.json");
+	auto entity = fight->teams[0]->entities[0];
+	test_ai(fight, entity, "Fight.getEntity().name", "'Poireau'");
+	test_ai(fight, entity, "Fight.getEntity().life", "500");
+	test_ai(fight, entity, "Fight.getEntity().weapons", "[<Weapon m_laser>]");
+	test_ai(fight, entity, "Fight.getEntity().chips", "[<Chip protein>]");
 }
 
 void Test::test_generateCritical() {
