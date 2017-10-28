1ff3411b35719c2ce01c5bab3a62e85385af7f03
Pilow <pierre.laupretre@gmail.com>
2017-10-28 22:20:36 +0200

    [effect] Remove launched effect in case of unstackable effect

--- src/effect/Effect.cpp
+++ src/effect/Effect.cpp
@@ -74,6 +74,7 @@ void Effect::createEffect(Fight* fight, EffectType type, int turns, double power
 			for (unsigned i = 0; i < effects.size(); ++i) {
 				auto e = (Effect*) effects[i];
 				if (e->attackID == attack_id && e->type == type) {
+					e->caster->remove_launched_effect(e);
 					target->removeEffect(e);
 					break;
 				}
