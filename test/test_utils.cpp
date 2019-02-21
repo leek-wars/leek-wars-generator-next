98b99e844b9768e1cfa33e737817ed5813e0256b
Pilow <pierre.laupretre@gmail.com>
2019-02-21 14:25:55 +0800

    [test][utils] Disabled output file

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -1,11 +1,9 @@
 #include "Test.hpp"
-#include <istream>
-#include <fstream>
 #include "../src/area/Area.hpp"
 #include "../src/effect/Attack.hpp"
 
 void Test::run_fight_browser(Report* report) {
-	std::ofstream report_file("fight.json");
-	report_file << report << std::endl;
+	// std::ofstream report_file("fight.json");
+	// report_file << report << std::endl;
 	system("chromium-browser http://localhost:8012/fight/local");
 }
