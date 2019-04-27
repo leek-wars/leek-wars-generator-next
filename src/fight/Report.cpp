6e65b7550ba53f0cb5a9b549fc8ddf9d9a36b4d4
Pilow <pierre.laupretre@gmail.com>
2019-04-27 10:34:04 +0800

    [report] Wrap the rapport in 'fight' attribute

--- src/fight/Report.cpp
+++ src/fight/Report.cpp
@@ -11,12 +11,14 @@ Report::~Report() {}
 
 std::ostream& operator << (std::ostream& os, const Report* report) {
 
-	Json json = {
-		{"actions", report->actions->json()},
-		{"map", report->fight->field->json()},
-		{"leeks", report->fight->entities_json()},
-		{"logs", report->fight->actions.logs}
-	};
+	Json json = {{"fight", 
+		{
+			{"actions", report->actions->json()},
+			{"map", report->fight->field->json()},
+			{"leeks", report->fight->entities_json()},
+			{"logs", report->fight->actions.logs}
+		}
+	}};
 
 	os << json;
 
