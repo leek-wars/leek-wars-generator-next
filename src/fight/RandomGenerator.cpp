28ca4eba236bd536d2b66210f1a9928799ee0eee
Pilow <pierre.laupretre@gmail.com>
2019-05-24 20:18:11 +0200

    [random] Comment logs

--- src/fight/RandomGenerator.cpp
+++ src/fight/RandomGenerator.cpp
@@ -6,7 +6,7 @@ RandomGenerator::RandomGenerator() {
 	n = 0;
 }
 void RandomGenerator::seed(long seed) {
-	LOG << "Random seed = " << seed << std::endl;
+	// LOG << "Random seed = " << seed << std::endl;
 	n = seed;
 }
 
@@ -17,7 +17,7 @@ int RandomGenerator::getInt(int min, int max) {
 }
 
 double RandomGenerator::getDouble() {
-	LOG << "n = " << n << std::endl;
+	// LOG << "n = " << n << std::endl;
 	n = n * 1103515245 + 12345;
 	return (double) ((n / 65536) % 32768 + 32768) / 65536;
 }
