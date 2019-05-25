d28746313f7280112908635dbf5811a556ede0c5
Pilow <pierre.laupretre@gmail.com>
2019-05-25 22:30:27 +0200

    [item] Add USE_SUCCESS constant in Item module

--- src/module/ItemModule.cpp
+++ src/module/ItemModule.cpp
@@ -1,5 +1,8 @@
 #include "ItemModule.hpp"
 
-ItemModule::ItemModule() : Module("Item") {}
+ItemModule::ItemModule() : Module("Item") {
+
+	static_field("USE_SUCCESS", ls::Type::integer(), [](ls::Compiler& c) { return c.new_integer(1); });
+}
 
 ItemModule::~ItemModule() {}
